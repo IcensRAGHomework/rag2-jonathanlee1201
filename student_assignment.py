5724bb1ba407c5a4e7a03c3abe7b19526e453e43
jonathanlee1201 <jonathanlee1201@gmail.com>
2025-02-14 17:30:02 +0800

    homework01

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,22 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    documents = loader.load()
+    splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=0)
+    chunks = splitter.split_documents(documents)
+
+    if chunks:
+        last_chunk = chunks[-1]
+        print("最後一個chunk的內容:")
+        print(last_chunk.page_content)
+    else:
+        print("未能分割PDF文件。")
 
 def hw02_2(q2_pdf):
     pass
+
+if __name__ == "__main__":
+    hw02_1(q1_pdf)
+
+
