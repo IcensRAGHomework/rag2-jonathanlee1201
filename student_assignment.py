1d55d0f47003067c9b81390aa44aae8a684bfb52
jonathanlee1201 <jonathanlee1201@gmail.com>
2025-02-19 16:19:48 +0800

    homework03

--- student_assignment.py
+++ student_assignment.py
@@ -23,9 +23,36 @@ def hw02_1(q1_pdf):
    
 
 def hw02_2(q2_pdf):
-    pass
+    loader = PyPDFLoader(q2_pdf)
+    documents = loader.load()
+
+    full_text = "\n".join(doc.page_content for doc in documents)
+
+    #splitter = RecursiveCharacterTextSplitter(["第","章", "條","\n", "\n\n", " "], 200, 10)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=90, 
+        chunk_overlap=0,  
+        separators=[
+            r"第\s*.+\s*章",
+            r"第\s*\d+-?\d*\s*條"
+        ],
+        is_separator_regex=True  # 啟用正則表達式
+    )
+    
+    chunks = text_splitter.split_text(full_text)
+
+    if chunks:
+        number_of_chunks = len(chunks)
+        print(f"總共有 {number_of_chunks} 個 chunks\n")
+
+        for i in range(number_of_chunks):
+            print(f"Chunk {i+1}:\n {chunks[i]}")
+    else:
+        print("未能分割PDF文件。")
+
+    return number_of_chunks
 
 if __name__ == "__main__":
-    hw02_1(q1_pdf)
+    hw02_2(q2_pdf)
 
 
